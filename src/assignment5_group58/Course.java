a1c32fc12e1a402570b3005ecc53cbdf8e3ab4bf
Kaustubh Hiware <hiwarekaustubh@googlemail.com>
2016-03-27 19:16:01 +0530

    Added course file - first commit from netbeans

---
+++ src/assignment5_group58/Course.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment5_group58;
+
+/**
+ *
+ * @author kaustubh
+ */
+import java.util.ArrayList;
+import java.io.Serializable;
+import java.util.Date;
+
+public class Course implements Serializable
+{
+	private String coursename;
+	private int credit;
+	private int grade;
+	private String status;
+	private String professor;
+	public  ArrayList<String> StudentsOnRoll = new ArrayList<String>();
+	
+	public void setname(String topic)
+	{
+		this.coursename = topic;
+	};
+	public String getname()
+	{
+		return coursename;
+	};
+	
+	public void setcredit(int allot_credit)
+	{
+		this.credit = allot_credit;
+	};
+	public int getcredit()
+	{
+		return credit;
+	};
+	
+	public void setgrade(int marks)
+	{
+		this.grade = marks;
+	};
+	public int getgrade()
+	{
+		return grade;
+	};
+	
+	public void setprofessor(String faculty)
+	{
+		this.professor = faculty;
+	};
+	
+	public String getprofessor()
+	{
+		return professor;
+	};
+
+}
+
